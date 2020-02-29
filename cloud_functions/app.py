6200796033d038c5920ab472c60e4c7933e9f1ce
Branislav Pilnan <B.Pilnan@gmail.com>
2020-02-29 17:20:14 +0000

    Add flask app for testing cloud functions

---
+++ cloud_functions/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+def hello_world(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    request_json = request.get_json()
+    if request_json and 'text' in request_json:
+        return jsonify(topic='abc', sentiment=len(request_json['text']))
+    else:
+        return f'Hello World!'
+
+
+@app.route('/', methods=['POST'])
+def call():
+    return hello_world(request)
+
+
+if __name__ == '__main__':
+    app.run()
