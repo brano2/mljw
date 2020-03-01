69931640f64ec1280d036c90905f950b33937906
Branislav Pilnan <B.Pilnan@gmail.com>
2020-03-01 04:40:02 +0000

    Start implementing article search [WIP]

--- cloud_functions/analyze_text.py
+++ cloud_functions/analyze_text.py
@@ -1,8 +1,11 @@
+import os
+
 from flask import jsonify
-import pandas as pd
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
+from newsapi import NewsApiClient
+import pandas as pd
 
 
 def process_annotations(annotations):
@@ -20,7 +23,20 @@ def process_annotations(annotations):
     df = pd.DataFrame({"content": content, "salience": salience, "sentiment": sentiment, "magnitude": magnitude})
     df = df.iloc[0:20].sort_values(['magnitude'], ascending=False)
 
-    return df.to_json()
+    return df
+
+def _build_query(phrases):
+    return ' '.join([f'"{phrase}"' if ' ' in phrase else phrase for phrase in phrases])
+
+def find_related_articles(df):
+    newsapi = NewsApiClient(api_key=os.getenv('NEWSAPI_KEY'))
+    n_keywords = 5
+    articles = []
+    while len(articles) < 10:
+        query = _build_query(df.content[:n_keywords])
+        search_results = newsapi.get_everything(q=query, language='en', sort_by='relevancy')
+        articles.extend(search_results['articles'])
+        n_keywords -= 1
 
 
 def get_annotations(text):
@@ -40,6 +56,6 @@ def analyze_text(request):
     if request_json and 'text' in request_json:
         annotations = get_annotations(request_json['text'])
         result = process_annotations(annotations)
-        return jsonify(result)
+        return jsonify(result.to_json())
     else:
         return 'error'
